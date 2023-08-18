dbd2968505f652fa781bdf7c649cd449b7dbbd21
ErikSilva231 <erik.silva.fuentes@gmail.com>
2023-08-17 21:41:30 -0400

    responsivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
 import Formulario from './components/Formulario'
 import { BaseColaboradores } from "../src/assets/js/BaseColaboradores.js";
 import Listado from './components/Listado';
@@ -13,12 +15,20 @@ function App() {
   const [listaoriginal, setListaOriginal] = useState(BaseColaboradores);
 
   return (
-    <>
+  
+    <div className="container">
     <h1>Lista de colaboradores</h1>
     <Buscador lista={lista} setLista={setLista} listaoriginal={listaoriginal}/>
-    <Listado lista={lista} setLista={setLista} />
+    <div className="row">
+    <div className="col-md-8">    
+      <Listado lista={lista} setLista={setLista} />
+      </div>
+      <div className="col-md-4">
     <Formulario lista={lista} setLista={setLista} color={color} setColor={setColor} error={error} setError={setError}/>
-    </>
+    </div>
+    </div>
+    </div>
+    
   )
 }
 
